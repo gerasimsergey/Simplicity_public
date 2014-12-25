5ffdeec8a71877ae61d41143cad8dff0f706d17d
Evgeny Baskakov <evgeny.baskakov@gmail.com>
2014-12-25 15:26:37 -0500

    Folder color marks appearance improved

--- Simplicity/SMColorCircle.m
+++ Simplicity/SMColorCircle.m
@@ -15,17 +15,18 @@
 	
 	NSRect rect = self.bounds;
 
-#if 0 // enable to draw the enclosing rect
-	[[NSColor grayColor] set];
-	[[NSBezierPath bezierPathWithRect:rect] fill];
-#endif
-
 	CGFloat spotSide = rect.size.width/2.0;
-	NSPoint point = NSMakePoint(NSMidX(rect), NSMidY(rect));
-	NSRect spotRect = NSMakeRect(point.x - spotSide/2 + 1, point.y - spotSide/2 + 2, spotSide, spotSide);
+	NSPoint point = NSMakePoint(NSMidX(rect) - spotSide/2, NSMidY(rect) - spotSide/2);
+
+	NSRect outerSpotRect = NSMakeRect(point.x, point.y + 1, spotSide + 2, spotSide + 2);
+	
+	[[NSColor grayColor] set];
+	[[NSBezierPath bezierPathWithOvalInRect: outerSpotRect] fill];
+
+	NSRect innerSpotRect = NSMakeRect(point.x + 1, point.y + 2, spotSide, spotSide);
 
 	[[NSColor blackColor] set];
-	[[NSBezierPath bezierPathWithOvalInRect: spotRect] fill];
+	[[NSBezierPath bezierPathWithOvalInRect: innerSpotRect] fill];
 }
 
 @end
