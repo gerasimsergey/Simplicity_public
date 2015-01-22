8e60bb48af127479a0390059d7ae093ff4148a09
Evgeny Baskakov <evgeny.baskakov@gmail.com>
2015-01-22 00:41:31 -0500

    Making attachments selectable

--- Simplicity/SMAttachmentItemView.m
+++ Simplicity/SMAttachmentItemView.m
@@ -8,15 +8,37 @@
 
 #import "SMAttachmentItemView.h"
 
-@interface SMAttachmentItemView ()
-
-@end
-
 @implementation SMAttachmentItemView
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do view setup here.
+- (void) awakeFromNib {
+	NSBox *view = (NSBox*)[self view];
+
+	[view setTitlePosition:NSNoTitle];
+	[view setBoxType:NSBoxCustom];
+	[view setCornerRadius:8.0];
+	[view setBorderType:NSLineBorder];
+}
+
+- (void)setSelected:(BOOL)flag {
+//	[super setSelected: flag];
+	
+//	NSBox *view = (NSBox*) [self view];
+
+ /*
+	NSColor *color;
+	NSColor *lineColor;
+ 
+	if (flag) {
+		color       = [NSColor selectedControlColor];
+		lineColor   = [NSColor blackColor];
+	} else {
+		color       = [NSColor controlBackgroundColor];
+		lineColor   = [NSColor controlBackgroundColor];
+	}
+	[view setBorderColor:lineColor];
+  [view setFillColor:color];
+  */
+//	NSLog(@"%s: view %@", __func__, view);
 }
 
 @end
