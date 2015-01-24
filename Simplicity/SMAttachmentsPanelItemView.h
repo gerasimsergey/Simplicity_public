acbbac71c87e74d8d0033614526c62f3615fd153
Evgeny Baskakov <evgeny.baskakov@gmail.com>
2015-01-24 00:46:16 -0500

    Attachment Item moved to a separate xib

---
+++ Simplicity/SMAttachmentsPanelItemView.h
@@ -0,0 +1,15 @@
+//
+//  SMAttachmentsPanelItemView.h
+//  Simplicity
+//
+//  Created by Evgeny Baskakov on 1/24/15.
+//  Copyright (c) 2015 Evgeny Baskakov. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface SMAttachmentsPanelItemView : NSCollectionViewItem
+
+@property IBOutlet NSTextField *fileNameField;
+
+@end
---
+++ Simplicity/SMAttachmentsPanelItemView.m
@@ -0,0 +1,22 @@
+//
+//  SMAttachmentsPanelItemView.m
+//  Simplicity
+//
+//  Created by Evgeny Baskakov on 1/24/15.
+//  Copyright (c) 2015 Evgeny Baskakov. All rights reserved.
+//
+
+#import "SMAttachmentsPanelItemView.h"
+
+@interface SMAttachmentsPanelItemView ()
+
+@end
+
+@implementation SMAttachmentsPanelItemView
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+}
+
+@end
