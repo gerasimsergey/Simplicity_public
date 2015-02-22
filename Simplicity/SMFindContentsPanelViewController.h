18fbbde54077afd898c600d87c5f0402fee4cc75
Evgeny Baskakov <evgeny.baskakov@gmail.com>
2015-02-22 01:15:33 -0500

    "Find contents" panel draft added

---
+++ Simplicity/SMFindContentsPanelViewController.h
@@ -0,0 +1,13 @@
+//
+//  SMSearchContentsPanelViewController.h
+//  Simplicity
+//
+//  Created by Evgeny Baskakov on 2/22/15.
+//  Copyright (c) 2015 Evgeny Baskakov. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface SMFindContentsPanelViewController : NSViewController
+
+@end
---
+++ Simplicity/SMFindContentsPanelViewController.m
@@ -0,0 +1,22 @@
+//
+//  SMSearchContentsPanelViewController.m
+//  Simplicity
+//
+//  Created by Evgeny Baskakov on 2/22/15.
+//  Copyright (c) 2015 Evgeny Baskakov. All rights reserved.
+//
+
+#import "SMFindContentsPanelViewController.h"
+
+@interface SMFindContentsPanelViewController ()
+
+@end
+
+@implementation SMFindContentsPanelViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+}
+
+@end
