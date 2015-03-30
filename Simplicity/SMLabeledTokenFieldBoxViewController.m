35f875cc24c384920d60515ebff5d3aead24fcaf
Evgeny Baskakov <evgeny.baskakov@gmail.com>
2015-03-29 23:50:50 -0400

    Labeled token field box now shows actual strings

--- Simplicity/SMLabeledTokenFieldBoxViewController.m
+++ Simplicity/SMLabeledTokenFieldBoxViewController.m
@@ -11,7 +11,7 @@
 #import "SMLabeledTokenFieldBoxViewController.h"
 
 @implementation SMLabeledTokenFieldBoxViewController {
-	Boolean _tokenFieldFramesValid;
+	Boolean _tokenFieldFrameValid;
 }
 
 - (void)viewDidLoad {
@@ -23,21 +23,21 @@
 	NSAssert([view isKindOfClass:[SMLabeledTokenFieldBoxView class]], @"view not SMLabeledTokenFieldBoxView");
 	
 	[(SMLabeledTokenFieldBoxView*)view setViewController:self];
-	
+
 	[(NSBox*)view setBoxType:NSBoxCustom];
 	[(NSBox*)view setTitlePosition:NSNoTitle];
 	[(NSBox*)view setFillColor:[NSColor whiteColor]];
 }
 
 - (void)viewDidAppear {
-	if(!_tokenFieldFramesValid) {
+	if(!_tokenFieldFrameValid) {
 		// this is critical because the frame height for each SMTokenField must be
 		// recalculated after its width is known, which happens when it is drawn
 		// for the first time
 
 		[_tokenField invalidateIntrinsicContentSize];
 
-		_tokenFieldFramesValid = YES;
+		_tokenFieldFrameValid = YES;
 	}
 }
 
@@ -51,119 +51,41 @@
 
 #pragma mark - NSTokenFieldDelegate
 
-// ---------------------------------------------------------------------------
-//	styleForRepresentedObject:representedObject
-//
-//	Make sure our tokens are rounded.
-//	The delegate should return:
-//		NSDefaultTokenStyle, NSPlainTextTokenStyle or NSRoundedTokenStyle.
-// ---------------------------------------------------------------------------
-- (NSTokenStyle)tokenField:(NSTokenField *)tokenField styleForRepresentedObject:(id)representedObject
-{
-	//	NSLog(@"%s", __func__);
+- (NSTokenStyle)tokenField:(NSTokenField *)tokenField styleForRepresentedObject:(id)representedObject {
+	//NSLog(@"%s", __func__);
 	return NSRoundedTokenStyle;
 }
 
-// ---------------------------------------------------------------------------
-//	hasMenuForRepresentedObject:representedObject
-//
-//	Make sure our tokens have a menu. By default tokens have no menus.
-// ---------------------------------------------------------------------------
-- (BOOL)tokenField:(NSTokenField *)tokenField hasMenuForRepresentedObject:(id)representedObject
-{
-	//	NSLog(@"%s", __func__);
+- (BOOL)tokenField:(NSTokenField *)tokenField hasMenuForRepresentedObject:(id)representedObject {
+	//NSLog(@"%s", __func__);
 	return NO;
 }
 
-// ---------------------------------------------------------------------------
-//	menuForRepresentedObject:representedObject
-//
-//	User clicked on a token, return the menu we want to represent for our token.
-//	By default tokens have no menus.
-// ---------------------------------------------------------------------------
-- (NSMenu *)tokenField:(NSTokenField *)tokenField menuForRepresentedObject:(id)representedObject
-{
-	NSLog(@"%s", __func__);
+- (NSMenu *)tokenField:(NSTokenField *)tokenField menuForRepresentedObject:(id)representedObject {
+	//NSLog(@"%s", __func__);
 	return nil;
 }
 
-// ---------------------------------------------------------------------------
-//	shouldAddObjects:tokens:index
-//
-//	Delegate method to decide whether the given token list should be allowed,
-//	we can selectively add/remove any token we want.
-//
-//	The delegate can return the array unchanged or return a modified array of tokens.
-//	To reject the add completely, return an empty array.  Returning nil causes an error.
-// ---------------------------------------------------------------------------
-- (NSArray *)tokenField:(NSTokenField *)tokenField shouldAddObjects:(NSArray *)tokens atIndex:(NSUInteger)index
-{
-	NSLog(@"%s", __func__);
-	return nil;
-	/*
-	 NSMutableArray *newArray = [NSMutableArray arrayWithArray:tokens];
-	 
-	 id aToken;
-	 for (aToken in newArray)
-	 {
-		if ([[aToken description] isEqualToString:self.tokenTitleToAdd])
-		{
-	 MyToken *token = [[MyToken alloc] init];
-	 token.name = [aToken description];
-	 [newArray replaceObjectAtIndex:index withObject:token];
-	 break;
-		}
-	 }
-	 
-	 return newArray;
-	 */
-}
-
-// ---------------------------------------------------------------------------
-//	completionsForSubstring:substring:tokenIndex:selectedIndex
-//
-//	Called 1st, and again every time a completion delay finishes.
-//
-//	substring =		the partial string that to be completed.
-//	tokenIndex =	the index of the token being edited.
-//	selectedIndex = allows you to return by-reference an index in the array
-//					specifying which of the completions should be initially selected.
-// ---------------------------------------------------------------------------
-- (NSArray *)tokenField:(NSTokenField *)tokenField completionsForSubstring:(NSString *)substring indexOfToken:(NSInteger)tokenIndex
-	indexOfSelectedItem:(NSInteger *)selectedIndex
-{
-	NSLog(@"%s", __func__);
+- (NSArray *)tokenField:(NSTokenField *)tokenField shouldAddObjects:(NSArray *)tokens atIndex:(NSUInteger)index {
+	//NSLog(@"%s", __func__);
+	// TODO: scan address books for the recepient name and/or verify the email address for correctness
 	return nil;
 }
 
-// ---------------------------------------------------------------------------
-//	representedObjectForEditingString:editingString
-//
-//	Called 2nd, after you choose a choice from the menu list and press return.
-//
-//	The represented object must implement the NSCoding protocol.
-//	If your application uses some object other than an NSString for their represented objects,
-//	you should return a new instance of that object from this method.
-//
-// ---------------------------------------------------------------------------
-- (id)tokenField:(NSTokenField *)tokenField representedObjectForEditingString:(NSString *)editingString
-{
-	NSLog(@"%s", __func__);
-	return @"Wilma";
+- (NSArray *)tokenField:(NSTokenField *)tokenField completionsForSubstring:(NSString *)substring indexOfToken:(NSInteger)tokenIndex indexOfSelectedItem:(NSInteger *)selectedIndex {
+	//NSLog(@"%s", __func__);
+	// TODO: scan address books for the recepient name
+	return nil;
 }
 
-// ---------------------------------------------------------------------------
-//	displayStringForRepresentedObject:representedObject
-//
-//	Called 3rd, once the token is ready to be displayed.
-//
-//	If you return nil or do not implement this method, then representedObject
-//	is displayed as the string. The represented object must implement the NSCoding protocol.
-// ---------------------------------------------------------------------------
-- (NSString *)tokenField:(NSTokenField *)tokenField displayStringForRepresentedObject:(id)representedObject
-{
-	//	NSLog(@"%s", __func__);
-	return representedObject;
+- (id)tokenField:(NSTokenField *)tokenField representedObjectForEditingString:(NSString *)editingString {
+	// TODO: append a recepient name from address books
+	return editingString;
+}
+
+- (NSString *)tokenField:(NSTokenField *)tokenField displayStringForRepresentedObject:(id)representedObject {
+	//NSLog(@"%s", __func__);
+	return nil;
 }
 
 @end
